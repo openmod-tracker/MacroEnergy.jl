5bbf37788da119e2242aa1bb33fb09149a42196b
Anna Li <li.anna6389@gmail.com>
2025-09-14 18:04:40 -0400

    delete examples

--- ExampleSystems/multistage_with_cement_retrofit/run.jl
+++
@@ -1,6 +0,0 @@
-using MacroEnergy
-using Gurobi
-
-(system, model) = run_case(@__DIR__; 
-                    optimizer=Gurobi.Optimizer,
-                    optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3));
